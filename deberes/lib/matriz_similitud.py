c0dec7e6468de06c49e0fb92b73ce7cda3a9b0ed
rromom@est.ups.edu.ec <rromom@est.ups.edu.ec>
2020-05-22 16:54:54 -0500

    tabla tf-idf optimizada to do: hacer analisis vectorial

--- deberes/lib/matriz_similitud.py
+++ deberes/lib/matriz_similitud.py
@@ -89,6 +89,44 @@ def get_fii(docs):
     fii = map(lambda x: get_positions(x, docs), my_dict)
     return list(fii)
 
+def word_bag_frecuency(fii,palabras,documentos,ide):
+    tb_tf=pd.DataFrame(float(0), index=palabras, columns=['doc'+str(x) for x in range(len(abstracts))])
+    for i in fii:
+        con=0
+        for j in i:
+            if con!=0:
+                if ide==True:
+                    tb_tf._set_value(i[0],"doc"+str(j[0]),j[1]) #tabla tf
+                else:
+                    tb_tf._set_value(i[0],"doc"+str(j[0]),(1+ma.log(j[1],10))) #tabla wtf
+            con+=1
+    return tb_tf
+
+def get_df_idf(palabras,tb_tf,tb_wtf,ide):
+    df=pd.DataFrame(float(0), index=palabras, columns=['frecuency'])
+    for index, row in tb_tf.iterrows():
+        con=0
+        for i,ind in row.iteritems():
+            if ind != 0:
+                con+=1
+        if ide==True:
+            if con!=0:
+                op=ma.log((len(tb_wtf.columns)/con),10)
+                df._set_value(index,'frecuency',op)
+            else:
+                df._set_value(index,'frecuency',con)
+        else:
+            df._set_value(index,'frecuency',con)
+    return df
+
+def get_tf_idf(palabras,abstracts,tb_wtf,idf):
+    tb_tf_idf=pd.DataFrame(float(0), index=palabras, columns=['doc'+str(x) for x in range(len(abstracts))])
+    for index, row in tb_wtf.iterrows():
+        for i,ind in row.iteritems():
+            #index nombre fila , # i columna nombre, #ind term frecuency
+            tb_tf_idf._set_value(index,i,(ind*idf._get_value(index,'frecuency')))
+    return tb_tf_idf
+
 
 if __name__ == "__main__":
     data = pd.read_csv('C:/Users/Ricardo/Desktop/dieguillo/python/ulti/dato.csv')
@@ -114,40 +152,15 @@ if __name__ == "__main__":
     palabras=[]
     for i in fii:
         palabras.append(i[0])
-    tb_wtf=pd.DataFrame(float(0), index=palabras, columns=['doc'+str(x) for x in range(len(abstracts))])
-    tb_tf=pd.DataFrame(float(0), index=palabras, columns=['doc'+str(x) for x in range(len(abstracts))])
-    for i in fii:
-        con=0
-        for j in i:
-            if con!=0:
-                tb_tf._set_value(i[0],"doc"+str(j[0]),j[1]) #tabla tf
-            con+=1
+    tf=word_bag_frecuency(fii,palabras,abstracts,True) #term frecuency
+    wtf=word_bag_frecuency(fii,palabras,abstracts,False) #weight term frecuency
+    # print(tf)
+    # print(wtf)
     
-    con=0
-    #print(tb_tf)
-    for index, row in tb_tf.iterrows():
-        for i,ind in row.iteritems():
-            if ind != 0:
-                #print(index," ",i,(1+ma.log(ind,10)))#index nombre fila , # i columna nombre, #ind term frecuency
-                tb_wtf._set_value(index,i,(1+ma.log(ind,10))) # tabla wtf
-    
-    idf=pd.DataFrame(float(0), index=palabras, columns=['frecuency'])
-    for index, row in tb_tf.iterrows():
-        con=0
-        for i,ind in row.iteritems():
-            if ind != 0:
-                con+=1
-        if con!=0:
-            op=ma.log((len(tb_wtf.columns)/con),10)
-            #print(op)
-            idf._set_value(index,'frecuency',op)
-        else:
-            idf._set_value(index,'frecuency',con)
-    
-    tb_tf_idf=pd.DataFrame(float(0), index=palabras, columns=['doc'+str(x) for x in range(len(abstracts))])
-    for index, row in tb_wtf.iterrows():
-        for i,ind in row.iteritems():
-            #index nombre fila , # i columna nombre, #ind term frecuency
-            tb_tf_idf._set_value(index,i,(ind*idf._get_value(index,'frecuency')))
-            #print((index," ",i," ",ind,"x",idf._get_value(index,'frecuency'),"=",(ind*idf._get_value(index,'frecuency'))))
+    df=get_df_idf(palabras,tf,wtf,False) # document frecuency
+    idf=get_df_idf(palabras,tf,wtf,True) # invert document frecuency
+    # print(df)
+    # print(idf)
+
+    tb_tf_idf=get_tf_idf(palabras,abstracts,wtf,idf)
     print(tb_tf_idf)
