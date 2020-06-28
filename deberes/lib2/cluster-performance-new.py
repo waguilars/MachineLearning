21a45411269bc25cd6d795e9a2c6b431239804a9
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-06-28 15:43:50 -0500

    external validartions

--- deberes/lib2/cluster-performance-new.py
+++ deberes/lib2/cluster-performance-new.py
@@ -11,7 +11,8 @@ from validclust.indices import dunn, silhouette_score
 from sklearn.cluster import KMeans, AgglomerativeClustering
 from sklearn.datasets import load_iris
 import numpy as np
-from sklearn.metrics import calinski_harabasz_score, pairwise_distances
+from sklearn.metrics import (pairwise_distances, adjusted_mutual_info_score,
+                             adjusted_rand_score, normalized_mutual_info_score)
 
 
 dataset = load_iris()
@@ -26,15 +27,22 @@ iris = np.array(dataset.data)
 print('-----------    KMeans    ------------')
 print('Indices de validacion interna ')
 kmeans = KMeans(n_clusters=3, random_state=0).fit(iris)
-labels = kmeans.fit_predict(iris)
+kmeans_labels = kmeans.fit_predict(iris)
 dist = pairwise_distances(iris)
 
-dunn_index = dunn( dist, labels)
+dunn_index = dunn(dist, kmeans_labels)
 print('Indice de dunn: ', dunn_index)
 
-silhouette_index = silhouette_score(iris, labels)
+silhouette_index = silhouette_score(iris, kmeans_labels)
 print('Indice de silueta: ', silhouette_index)
 
+print('\nIndices de validacion externa:')
+nmi = normalized_mutual_info_score(load_iris().target, kmeans_labels)
+ami = adjusted_mutual_info_score(load_iris().target, kmeans_labels)
+ari = adjusted_rand_score(load_iris().target, kmeans_labels)
+print('NMI: ', nmi)
+print('AMI: ', ami)
+print('ARI: ', ari)
 
 #############################################
 #			            DHC
@@ -49,3 +57,11 @@ print('Indice de dunn: ', dunn_index)
 
 silhouette_index = silhouette_score(iris, dhc_labels)
 print('Indice de silueta: ', silhouette_index)
+
+print('\nIndices de validacion externa:')
+nmi = normalized_mutual_info_score(load_iris().target, dhc_labels)
+ami = adjusted_mutual_info_score(load_iris().target, dhc_labels)
+ari = adjusted_rand_score(load_iris().target, dhc_labels)
+print('NMI: ', nmi)
+print('AMI: ', ami)
+print('ARI: ', ari)
