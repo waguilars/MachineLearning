0476533c9fdd8fd03465e54a740112f5772fa62c
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-07-02 20:50:14 -0500

    deber 9 finished

--- deberes/lib2/internal-validation-papers.py
+++ deberes/lib2/internal-validation-papers.py
@@ -9,13 +9,27 @@ import math as ma
 import pandas as pd
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering
-
+from sklearn.metrics import pairwise_distances
+from scipy.cluster.hierarchy import dendrogram, linkage
+import matplotlib.pyplot as plt
+from validclust.indices import dunn, silhouette_score
 
 mtx = pd.read_pickle('mtx')
 
 dhc = AgglomerativeClustering(n_clusters=5, linkage='complete')
 labels = dhc.fit_predict(mtx)
-print(labels)
+
+dendrogram = dendrogram(linkage(mtx, method='complete'))
+model = AgglomerativeClustering(
+    n_clusters=5, affinity='euclidean', linkage='complete')
+
+plt.axhline(y=3.5, c='k')
+plt.show()
 
 
+dunn_index = dunn(mtx.to_numpy(), labels)
+print('Indice de dunn: ', dunn_index)
+
+silhouette_index = silhouette_score(mtx, labels)
+print('Indice de silueta: ', silhouette_index)
 
