a3e5ee6dc59a278a5da5d4f69d0bcb35d20f5b0d
rromom <rromom@est.ups.edu.ec>
2020-07-12 21:29:04 -0500

    mae mse rmse r2 and r2a

--- deberes/lib2/regresion-performance.py
+++ deberes/lib2/regresion-performance.py
@@ -1,3 +1,12 @@
+"""
+Integrantes:
+- Wilson Aguilar
+- Gabriel Cacuango
+- Christian Lasso
+- Ricardo Romo
+"""
+
+import math as ma
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -16,3 +25,33 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 regresion = LinearRegression().fit(X_train, y_train)
 r2 = regresion.score(X_test, y_test)
+
+y_predic= regresion.predict(X_test)
+
+
+
+mae = mean_absolute_error(y_test,y_predic)
+print("=================================")
+print("MAE:",mae)
+print("=================================\n")
+
+
+mse = mean_squared_error(y_test,y_predic)
+print("=================================")
+print("MSE:",mse)
+print("=================================\n")
+
+
+rmse = ma.sqrt(mse)
+print("=================================")
+print("RMSE:",rmse)
+print("=================================\n")
+
+print("=================================")
+print("R2:",r2)
+print("=================================\n")
+
+r2a = 1-(1-r2)*(len(y_predic)-1)/(len(y_predic)-len(regresion.coef_)-1)
+print("=================================")
+print("R2 Ajustado:",r2a)
+print("=================================\n")
