0db238cc2d55267a2446b3277dade2f37f6aa7aa
rromom <rromom@est.ups.edu.ec>
2020-07-12 21:03:10 -0500

    modelo y bondad de ajuste r2

--- deberes/lib2/regresion-performance.py
+++ deberes/lib2/regresion-performance.py
@@ -1,8 +1,12 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 x = load_iris().data[:, 0:3]
 y = load_iris().data[:, 3]
-print(x)
-print(y)
-print(load_iris().feature_names)
+
+
+X_train,X_test,y_train,y_test= train_test_split(x,y,test_size=0.3,random_state=0)
+
+regresion = LinearRegression().fit(X_train,y_train)
+r2 = regresion.score(X_test,y_test)
