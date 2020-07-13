4a6989d128586cc73b10fa137f249083727089d6
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-07-12 20:48:51 -0500

    regresion performance - select data

--- deberes/lib2/regresion-performance.py
+++ deberes/lib2/regresion-performance.py
@@ -1,11 +1,8 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 
-
-data = load_iris().data
-data1 = load_iris().target
-X, y = load_iris(return_X_y=True)
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=0)
+x = load_iris().data[:, 0:3]
+y = load_iris().data[:, 3]
+print(x)
+print(y)
+print(load_iris().feature_names)
