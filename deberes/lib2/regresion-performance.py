453b1a011777f457c6b8e07d588fa26a23cc68d6
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-07-12 20:41:09 -0500

    regresion performance - select data

--- deberes/lib2/regresion-performance.py
+++ deberes/lib2/regresion-performance.py
@@ -1,2 +1,8 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LinearRegression
+
+x = load_iris().data[:, 0:3]
+y = load_iris().data[:, 3]
+print(x)
+print(y)
+print(load_iris().feature_names)
