39a3fff9267a35dcd21691e41a23953b7afd209e
rromom <rromom@est.ups.edu.ec>
2020-07-12 20:36:44 -0500

    regresion performance X y test and traiding

--- deberes/lib2/regresion-performance.py
+++ deberes/lib2/regresion-performance.py
@@ -1,2 +1,11 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+
+
+data = load_iris().data
+data1 = load_iris().target
+X, y = load_iris(return_X_y=True)
+
+X_train,X_test,y_train,y_test= train_test_split(X,y,test_size=0.3,random_state=0)
